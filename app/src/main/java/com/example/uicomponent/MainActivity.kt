f9320d9765f189801e681963fd8ee6afe788e74e
ud13n13w <muhammadburhanuddinfawwaz@gmail.com>
2020-10-22 10:49:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uicomponent/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.uicomponent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Mendefinikan variabel pada komponen View
+        val imgBtn = findViewById<ImageButton>(R.id.imgButtonAdd)
+        imgBtn.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Berhasil", Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
+
